462d01bcc45d40dd110949a4f944727942d241bd
Mushfiq1010 <151173437+Mushfiq1010@users.noreply.github.com>
2024-10-05 10:06:26 +0000

    Var datatype

---
+++ 01_basic/01_var.js
@@ -0,0 +1,11 @@
+const accountId=144553
+var accountPass="12345"
+let accountEmail="mushi10@gmail.com"
+accountCity="Dacca"
+
+accountPass="123"
+accountEmail="dhrubo@gmail.com"
+accountCity="London"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity]);
